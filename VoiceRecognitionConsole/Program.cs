20fddcbab29b0f8b82e4d378a7c6a2b827753f56
yoshiki <tanayoshi78907890@icloud.com>
2022-10-23 00:28:43 +0900

    set speech sdk

--- VoiceRecognitionConsole/Program.cs
+++ VoiceRecognitionConsole/Program.cs
@@ -1,12 +1,49 @@
 ﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
 
-namespace HelloWorld
+class Program
 {
-    class Program
+    static void OutputSpeechRecognitionResult(SpeechRecognitionResult speechRecognitionResult)
     {
-        static void Main(string[] args)
+        switch (speechRecognitionResult.Reason)
         {
-            Console.WriteLine("Hello World!");
+            case ResultReason.RecognizedSpeech:
+                Console.WriteLine($"RECOGNIZED: Text={speechRecognitionResult.Text}");
+                break;
+            case ResultReason.NoMatch:
+                Console.WriteLine($"NOMATCH: Speech could not be recognized.");
+                break;
+            case ResultReason.Canceled:
+                var cancellation = CancellationDetails.FromResult(speechRecognitionResult);
+                Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                if (cancellation.Reason == CancellationReason.Error)
+                {
+                    Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                    Console.WriteLine($"CANCELED: ErrorDetails={cancellation.ErrorDetails}");
+                    Console.WriteLine($"CANCELED: Did you set the speech resource key and region values?");
+                }
+                break;
         }
+    }
+
+    async static Task Main(string[] args)
+    {
+        DotNetEnv.Env.Load();
+        // This example requires environment variables named "SPEECH_KEY" and "SPEECH_REGION"
+        string speechKey = DotNetEnv.Env.GetString("SPEECH_KEY");
+        string speechRegion = DotNetEnv.Env.GetString("SPEECH_REGION");
+        var speechConfig = SpeechConfig.FromSubscription(speechKey, speechRegion);
+        speechConfig.SpeechRecognitionLanguage = "en-US";
+
+        using var audioConfig = AudioConfig.FromDefaultMicrophoneInput();
+        using var speechRecognizer = new SpeechRecognizer(speechConfig, audioConfig);
+
+        Console.WriteLine("Speak into your microphone.");
+        var speechRecognitionResult = await speechRecognizer.RecognizeOnceAsync();
+        OutputSpeechRecognitionResult(speechRecognitionResult);
     }
 }
